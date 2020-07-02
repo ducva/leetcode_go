6153039081ad7fe1619954f92bcfb8fcfdbf02c5
Michael Vu <vuanhduc2685@gmail.com>
2020-07-02 10:50:45 +0700

    Update common. add ultilities

--- Common/main.go
+++ Common/main.go
@@ -7,3 +7,17 @@ func AssertEqual(expected, actual interface{}, t *testing.T) {
 		t.Errorf("Wrong: expected: %s, actual: %s", expected, actual)
 	}
 }
+
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
+func Abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
